44a21fc200507b40dc4a3846a077561a0f8dabe9
maja-98 <majajouhar@gmail.com>
2022-12-03 23:27:20 +0530

    minor UI fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,8 @@ export default function Home() {
       {overlay &&
       <div className='overlay'>
         <div className='overlay-message-box'>
-          <p className='overlay-message'>This site is not yet compltely developed. For Better User experience use PC as of Now app is incompatible on Mobile devices.
+          <p className='overlay-message'>This site is not yet compltely developed. For Better User experience use Desktop (You can try Desktop Mode in Mobile).
+          As of Now app is incompatible on Mobile devices.
           Also Many of the functionalities yet to build.Will deploy more features soon. Happy Listening!!!</p>
           <div className='overlay-button-container'>
             <button onClick={handleOverlay}>Start Listening</button>
